10a0713fafcc4789365eb1117703cef78edb83c2
Lexuil <ojumanab@unal.edu.co>
2023-07-25 16:25:40 -0500

    add tailwind

--- pruebas/01-reading-list/Lexuil/app.vue
+++ pruebas/01-reading-list/Lexuil/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <main class="flex items-center justify-center min-h-screen">
+    <h1 class="font-black text-5xl">Hello world</h1>
+  </main>
 </template>
